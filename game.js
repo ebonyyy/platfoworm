b6c6db7317a1f71972ff6c1d041183e58f6d66c6
ebonyyy <38299558+ebonyyy@users.noreply.github.com>
2020-02-12 17:59:38 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,41 +1,34 @@
 let hero = new Hero()
-let shapes = []
+let keyPressed = {}
 
-ERASE()
-hero.draw()
 
 window.addEventListener("keydown", event => {
+	keyPressed[event.code]=true
 	console.log(event.code)
-	if (event.code === 'KeyS'){
-	let colorIndex = Math.random()
-	let color = "red"
-	if (colorIndex <= 0.3) {
-		color = "blue"
-	}
-	else if ((colorIndex >0.3 ) && (colorIndex <=0.6)){
-		color = "purple"
-	}
-	else if ((colorIndex >0.6) && (colorIndex <= 0.9)){
-		color = "black"
-	}
-	else if (colorIndex >0.9) {
-		color = "pink"
-	}
-	CTX.fillStyle = color
-
-	let x = Math.random() * CANVAS.width
-	let y = Math.random() * CANVAS.height
-	let width = Math.random() * CANVAS.width
-	let height = Math.random() * CANVAS.height
-	let s = new Shape(x,y,width,height)
-	shapes.push(s)
-}
-if(event.code === "ArrowRight"){
-	hero.moveRight()
-}
-ERASE()
-
-shapes.forEach(s => s.draw())
-hero.draw()
 })
 
+window.addEventListener("keyup", event =>{
+	keyPressed[event.code]=false
+})
+
+function loop() {
+	if(keyPressed["ArrowRight"]){
+		hero.moveRight()
+	}
+	if(keyPressed["ArrowLeft"]){
+		hero.moveLeft()
+	}
+	if(keyPressed["ArrowUp"]){
+		hero.moveUp()
+	}
+	if(keyPressed["ArrowDown"]){
+		hero.moveDown()
+	}
+	ERASE()
+
+	
+	hero.draw()
+
+	setTimeout(loop, 1000/60)
+}
+loop()
